d4c5768ab8500bf52d089ce5e5740884ce6ef906
Петр Атанасов <79119574538@yandex.ru>
2023-06-10 23:59:43 +0300

    Hello, World!

--- English/MyForm.h
+++ English/MyForm.h
@@ -34,6 +34,9 @@ namespace English {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +51,48 @@ namespace English {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(98, 112);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(68, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Hello, World!";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(35, 154);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(219, 74);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"ghiuhgikjgkjng";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		
+	}
 	};
 }
