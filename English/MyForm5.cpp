2c9f50e524f24a9706e18256be36fc87638bff31
PC-06 <PC-06@DESKTOP-K9A53N3>
2023-06-17 11:43:27 +0300

    update button teoria

--- English/MyForm5.cpp
+++ English/MyForm5.cpp
@@ -1,2 +1,1 @@
 #include "MyForm5.h"
-
--- English/MyForm5.h
+++ English/MyForm5.h
@@ -1,5 +1,4 @@
 #pragma once
-#include"MyForm4.h"
 
 namespace English {
 
@@ -36,6 +35,7 @@ namespace English {
 			}
 		}
 	private: System::Windows::Forms::WebBrowser^ webBrowser1;
+	private: System::Windows::Forms::Button^ button1;
 	protected:
 
 	private:
@@ -53,6 +53,7 @@ namespace English {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm5::typeid));
 			this->webBrowser1 = (gcnew System::Windows::Forms::WebBrowser());
+			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// webBrowser1
@@ -64,6 +65,20 @@ namespace English {
 			this->webBrowser1->TabIndex = 16;
 			this->webBrowser1->Url = (gcnew System::Uri(L"V:\\C121\\Атанасов\\ПРАКТИКА\\Present Simple.html", System::UriKind::Absolute));
 			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(117)), static_cast<System::Int32>(static_cast<System::Byte>(111)),
+				static_cast<System::Int32>(static_cast<System::Byte>(140)));
+			this->button1->Font = (gcnew System::Drawing::Font(L"Consuela", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->ForeColor = System::Drawing::SystemColors::MenuBar;
+			this->button1->Location = System::Drawing::Point(24, 21);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(147, 46);
+			this->button1->TabIndex = 17;
+			this->button1->Text = L"Вернуться обратно";
+			this->button1->UseVisualStyleBackColor = false;
+			// 
 			// MyForm5
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
@@ -71,6 +86,7 @@ namespace English {
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
 			this->ClientSize = System::Drawing::Size(1211, 681);
+			this->Controls->Add(this->button1);
 			this->Controls->Add(this->webBrowser1);
 			this->Name = L"MyForm5";
 			this->Text = L"MyForm5";
