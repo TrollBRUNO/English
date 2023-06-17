6f67597d904f67a755511ad90d8db89d1391da35
PC-06 <PC-06@DESKTOP-K9A53N3>
2023-06-17 10:17:42 +0300

    add browser form in window tense(no func)

--- English/MyForm4.h
+++ English/MyForm4.h
@@ -38,6 +38,7 @@ namespace English {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::WebBrowser^ webBrowser1;
 	protected:
 
 	private:
@@ -58,6 +59,7 @@ namespace English {
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->webBrowser1 = (gcnew System::Windows::Forms::WebBrowser());
 			this->SuspendLayout();
 			// 
 			// label1
@@ -124,6 +126,15 @@ namespace English {
 			this->button3->Text = L"Future Simple";
 			this->button3->UseVisualStyleBackColor = false;
 			// 
+			// webBrowser1
+			// 
+			this->webBrowser1->Location = System::Drawing::Point(398, 452);
+			this->webBrowser1->MinimumSize = System::Drawing::Size(20, 20);
+			this->webBrowser1->Name = L"webBrowser1";
+			this->webBrowser1->Size = System::Drawing::Size(421, 168);
+			this->webBrowser1->TabIndex = 15;
+			this->webBrowser1->Url = (gcnew System::Uri(L"V:\\C121\\Àòàíàñîâ\\ÏÐÀÊÒÈÊÀ\\trash\\Present Simple.html", System::UriKind::Absolute));
+			// 
 			// MyForm4
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
@@ -131,6 +142,7 @@ namespace English {
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
 			this->ClientSize = System::Drawing::Size(1211, 681);
+			this->Controls->Add(this->webBrowser1);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
